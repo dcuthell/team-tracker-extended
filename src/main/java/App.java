7b6fd74533ad0b146554c167a4b14e4307e5426f
dcuthell <dcuthell@pdx.edu>
2017-08-11 13:37:05 -0700

    add hbs files and routes for creating a new team

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,52 @@
+import models.Team;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
+        //get: show new post form
+        get("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "team-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        //post: process new post form
+        post("/teams", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Team> teams = Team.getAllTeams();
+            String name = request.queryParams("name");
+            String description = request.queryParams("description");
+            String leader = request.queryParams("leader");
+            Team newTeam = new Team(name, description);
+            newTeam.addMember(leader);
+            model.put("team", newTeam);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+        //get: show all posts
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Team> teams = Team.getAllTeams();
+            model.put("teams", teams);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
+        //get: show an individual post
+
+        //get: show a form to update a post
+
+        //post: process a form to update a post
+
+        //get: delete an individual post
+
+        //get: delete all posts
     }
 }
